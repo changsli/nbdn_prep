4f4b61adab17f641b4a43e7ea0ee1172b2782b11
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-11-29 20:42:08 -0700

    Filtering cont..

---
+++ product/nothinbutdotnetprep/infrastructure/filtering/CriteriaFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace nothinbutdotnetprep.infrastructure.filtering
+{
+    public class CriteriaFactory<ItemToFilter, PropertyType>
+    {
+        Func<ItemToFilter, PropertyType> property_accessor;
+
+        public CriteriaFactory(Func<ItemToFilter, PropertyType> property_accessor)
+        {
+            this.property_accessor = property_accessor;
+        }
+
+        public Criteria<ItemToFilter> equal_to(PropertyType value_to_equal)
+        {
+            return new AnonymousCriteria<ItemToFilter>(x => property_accessor(x).Equals(value_to_equal));
+        }
+    }
+}
--- product/nothinbutdotnetprep/infrastructure/filtering/Where.cs
+++ product/nothinbutdotnetprep/infrastructure/filtering/Where.cs
@@ -2,12 +2,13 @@ using System;
 
 namespace nothinbutdotnetprep.infrastructure.filtering
 {
+
     public class Where<ItemToFilter>
     {
-        public static void has_a<PropertyType>(
+        public static CriteriaFactory<ItemToFilter,PropertyType> has_a<PropertyType>(
             Func<ItemToFilter, PropertyType> property_accessor)
         {
-
+            return new CriteriaFactory<ItemToFilter,PropertyType>(property_accessor);
         }
     }
 }
